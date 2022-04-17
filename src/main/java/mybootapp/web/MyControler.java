a35f47bd2957377d5543d332ff14e33f8bd635e4
RomeoC13 <44019336+RomeoC13@users.noreply.github.com>
2022-04-17 05:34:04 +0200

    Update MyControler.java

--- src/main/java/mybootapp/web/MyControler.java
+++ src/main/java/mybootapp/web/MyControler.java
@@ -1,5 +1,7 @@
 package mybootapp.web;
 
+import mybootapp.dao.DirectoryDao;
+import mybootapp.model.Group;
 import mybootapp.model.Person;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
@@ -8,10 +10,37 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.annotation.PostConstruct;
+
 @Controller
 @RequestMapping("/")
 public class MyControler {
-    Person person;
+    DirectoryDao dir;
+
+    @PostConstruct
+    public void init(){
+        dir = new DirectoryDao();
+
+        //TO TEST
+        Group testGroup = new Group("FSI");
+
+
+        Person per1 = new Person();
+        per1.setId(0);
+        per1.setFirstName("Roméo");
+        per1.setLastName("CHATEL");
+        per1.setEmailAddress("romeo@romeo.fr");
+        per1.setUserGroup(testGroup);
+        dir.addPerson(per1);
+
+        Person per2 = new Person();
+        per2.setId(1);
+        per2.setFirstName("Louis");
+        per2.setLastName("DE CAMPOU");
+        per2.setEmailAddress("louis@louis.com");
+        per2.setUserGroup(testGroup);
+        dir.addPerson(per2);
+    }
 
     @RequestMapping("")
     public String index() {
@@ -30,12 +59,18 @@ public class MyControler {
 
     @RequestMapping("/viewMail")
     public ModelAndView viewMail(@RequestParam String key){
-        return new ModelAndView("viewMail", "mail",key);
+        ModelAndView modelAndView = new ModelAndView("viewMail");
+        modelAndView.addObject("mailList",dir.searchPersonsByEmailAddress(key));
+        modelAndView.addObject("key", key);
+        return modelAndView;
     }
 
     @RequestMapping("/viewGroup")
     public ModelAndView viewGroup(@RequestParam String key){
-        return new ModelAndView("viewGroup", "group",key);
+        ModelAndView modelAndView=  new ModelAndView("viewGroup");
+        modelAndView.addObject("groupList",dir.searchGroupsByName(key));
+        modelAndView.addObject("key", key);
+        return modelAndView;
     }
 
 }
